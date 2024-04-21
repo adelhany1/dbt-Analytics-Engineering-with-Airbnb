9260bf24b40ddf410d463a948f82185994d2a2a9
Adel  Hany Abdelfatah <99558161+adelhany1@users.noreply.github.com>
2024-04-21 15:04:14 +0200

    Add files via upload

---
+++ target/run/dbtlearn/models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,44 @@
+
+  create or replace  view airbnb.dev.dim_listings_cleansed
+  
+   as (
+    
+
+WITH  __dbt__cte__src_listings as (
+WITH raw_listings AS (
+    SELECT
+        *
+    FROM
+        airbnb.raw.raw_listings
+)
+SELECT
+    id AS listing_id,
+    name AS listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price AS price_str,
+    created_at,
+    updated_at
+FROM
+    raw_listings
+),src_listings AS (
+    SELECT * FROM __dbt__cte__src_listings 
+)
+
+SELECT 
+  listing_id,
+  listing_name,
+  room_type,
+  CASE
+    WHEN minimum_nights = 0 THEN 1    -- the Min is 1 : at least 1 night need to be booked to do the reservations
+    ELSE minimum_nights
+  END AS minimum_nights,
+  host_id,
+  CAST(REPLACE(price_str, '$', '') AS NUMERIC(10,2)) AS price,  -- delete the $ and convert it to Number up to 2 decimals
+  created_at,
+  updated_at
+FROM
+  src_listings
+  );
